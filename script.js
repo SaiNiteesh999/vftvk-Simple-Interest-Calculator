31c6436941f0d068de205a873bd16e75f591ef7f
SaiNiteesh999 <110325433+SaiNiteesh999@users.noreply.github.com>
2022-07-31 12:26:12 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var mvalue = document.getElementById("mvalue").value;
+    var irate = document.getElementById("irate").value;
+     var year = document.getElementById("year").value;
+    if (mvalue == "") {
+        alert("Enter a positive number");
+        document.getElementById("mvalue").focus();
+        return false;
+    }else {
+        if (mvalue <= 0) {
+            alert("Enter a positive number");
+            document.getElementById("mvalue").focus();
+            return false;
+        }
+    }
+    var HDB = new Date().getFullYear();
+    var future= parseInt(HDB,10) + parseInt(year,10);
+    var interest= mvalue * year * irate / 100;
+    var result= "If you deposit <mark>" + mvalue + "</mark>,<br> at an interest rate of <mark>" + irate + "</mark>%. <br> You will receive an amount of <mark>" + interest + ",</mark><br> in the year <mark>" + future + "</mark>";
+    document.getElementById("result").innerHTML=result;
+}
+
+function showVal(newVal){ 
+    document.getElementById("InterestRate").innerHTML=newVal + '%';
 }
         
